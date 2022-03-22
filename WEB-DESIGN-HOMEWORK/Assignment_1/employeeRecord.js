187174d252f28f9768eae34c7f39150865478e73
Minesh6684 <minesh9174@gmail.com>
2022-03-22 17:51:31 -0400

    comment addition to Assignment1

--- WEB-DESIGN-HOMEWORK/Assignment_1/employeeRecord.js
+++ WEB-DESIGN-HOMEWORK/Assignment_1/employeeRecord.js
@@ -1,15 +1,26 @@
 function onReset() {
+    // obtaining values of input fields using id and assigning to variables 
     let fName = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let designation = document.getElementById('designation').value;
     let salary = document.getElementById('salary').value;
+    let phone_number = document.getElementById('cell').value
     
-    let recordArray = [fName, email, designation, salary];
+    // created array of the obtained data to make it iterable
+    let recordArray = [fName, email, designation, salary, phone_number];
+    
+    // row creation
     let tr = document.createElement('tr');
+    
+    /* with every iteration loop creates 'td' element, 
+    assign input field value to it and append the element to
+    row created */
     for (let i=0; i < recordArray.length; i++) {
         let td = document.createElement('td');
         td.innerHTML = recordArray[i];
         tr.append(td);
     }
+
+    // row will be appended to the tbody
     tbody.append(tr);
 }
