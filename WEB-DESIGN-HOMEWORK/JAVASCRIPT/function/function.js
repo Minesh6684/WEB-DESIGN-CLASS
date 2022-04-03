a3ffe91e16953656bf3cc96b51803bcb55b744cf
Minesh6684 <minesh9174@gmail.com>
2022-04-02 21:57:43 -0400

    use of setTimeout() as homework

--- WEB-DESIGN-HOMEWORK/JAVASCRIPT/function/function.js
+++ WEB-DESIGN-HOMEWORK/JAVASCRIPT/function/function.js
@@ -1,8 +1,7 @@
-// // 1. Calculate the sum of array of numbers: using reduce() method
-// nums = [10, 40, 50, 60];
-// let sum = nums.reduce((accumulator, currentValue) =>  accumulator + currentValue)
-// console.log(sum)
-
+// 1. Calculate the sum of array of numbers: using reduce() method
+var nums = [10, 40, 50, 60];
+let sum = nums.reduce((accumulator, currentValue) =>  accumulator + currentValue)
+console.log(sum);
 
 // 2. Create an array of objects as below:
 let object = [
@@ -22,7 +21,7 @@ let object = [
         Age: 23
     }
 
-]
+];
 
 // a. iterate through the object,
 // b. Diplay the name of the people on the UI(index.html)
@@ -30,20 +29,24 @@ let object = [
 document.write('People, whose age is less than or equal to 25:\n')
 for (let i=0; i<object.length; i++) {
     if (object[i].Age <= 25) {
-        document.write(`${object[i].Name}`)
+        document.write(`\n${object[i].Name}`);
     }
 }
 
-// 3. array.map() method
+/* 3. Multiply each element of nums array 
+and store result as an array to new array 
+using array.map() method */
 
-nums = [10, 40, 50, 60];
-let newArrayOfNums = nums.map( (currentValue) => currentValue*3(newArrayOfNums)
+var nums = [10, 40, 50, 60];
+let newArrayOfNums = nums.map( (currentValue) => currentValue*3 );
+console.log(newArrayOfNums);
 
 /* 4.Create new array of elements,
- where each array element when divided by 2, yields remaindie() */
+ where each array element when divided by 2, 
+ yields remainder 0. */
 
-nums = [10, 40, 50, 60, 5, 23, 24];
-const divisibleByTwo = Array()
+var nums = [10, 40, 50, 60, 5, 23, 24];
+const divisibleByTwo = Array();
 for (element of nums) {
     if (element%2 === 0) {
         divisibleByTwo.push(element);
@@ -51,11 +54,15 @@ for (element of nums) {
 }
 console.log(divisibleByTwo)
 
-// // 5. 
-// function numbers(j) {
-//     console.log(`${j} *(after 2 seconds)*`)
-// }
+/* 5. Using setTimeout(), print from 1 to 5,
+    wherre each element need to be display
+    after 2seconds delay.*/
 
-// for(let i=1; i<6; i++) {
-//     setTimeout(numbers(i), 2000)
-// }
+const printNumberAt2SecondsInterval = (endpoint) => {
+    for(let i=1; i<=endpoint; i++) {
+        setTimeout( () => {
+            console.log(`${i} *(after 2 seconds)*`)
+        }, i * 2000)
+    }
+}
+printNumberAt2SecondsInterval(5)
